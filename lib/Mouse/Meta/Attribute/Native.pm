a820ddbf6ae8a368f59f655be7508371883e0bbf
Fuji, Goro <gfuji@cpan.org>
2010-11-06 19:37:19 +0900

    Checking in changes prior to tagging of version 1.02.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 4895404..e4d3a80 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension MouseX::NativeTraits
    
    +1.02 2010-11-06 19:36:15
    +    - Fix testing issue on Windows/nmake
    +
     1.01 2010-11-05 20:27:52
         - Requires Mouse 0.82 for type constraint robusity
         - Follow Moose 1.19 except for smart coercions

--- lib/Mouse/Meta/Attribute/Native.pm
+++ lib/Mouse/Meta/Attribute/Native.pm
@@ -1,7 +1,7 @@
 package Mouse::Meta::Attribute::Native;
 use strict;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 1;
 __END__
--- lib/MouseX/NativeTraits.pm
+++ lib/MouseX/NativeTraits.pm
@@ -2,7 +2,7 @@ package MouseX::NativeTraits;
 use 5.006_002;
 use Mouse::Role;
 
-our $VERSION = '1.01';
+our $VERSION = '1.02';
 
 requires qw(method_provider_class helper_type);
 
@@ -84,7 +84,7 @@ MouseX::NativeTraits - Extend your attribute interfaces for Mouse
 
 =head1 VERSION
 
-This document describes MouseX::NativeTraits version 1.01.
+This document describes MouseX::NativeTraits version 1.02.
 
 =head1 SYNOPSIS
 
